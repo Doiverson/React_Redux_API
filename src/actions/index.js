df2a340b4384c3720a021720542fe8f034824ddf
Shosuke Doi <doiverson.canada@gmail.com>
2019-06-05 15:28:34 -0700

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../api/jsonPlaceholder';
+
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts())
+    const userIds = _.uniq(_.map(getState().posts, 'userId'))
+    userIds.forEach(id => dispatch(fetchUser(id)))
+}
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts')
@@ -10,6 +18,7 @@ export const fetchPosts = () => async dispatch => {
 }
 
 export const fetchUser = (id) => async dispatch => {
+
     const response = await jsonPlaceholder.get(`/users/${id}`)
 
     dispatch({
@@ -17,6 +26,18 @@ export const fetchUser = (id) => async dispatch => {
         payload: response.data
     })
 }
+
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch)
+//
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`)
+//
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     })
+// })
+
 
 
 
