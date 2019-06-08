93cfa6798feba5b20ed3c2e9e53c1cae14d1d407
Shosuke Doi <doiverson.canada@gmail.com>
2019-06-08 14:09:52 -0700

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,7 +18,6 @@ export const fetchPosts = () => async dispatch => {
 }
 
 export const fetchUser = (id) => async dispatch => {
-
     const response = await jsonPlaceholder.get(`/users/${id}`)
 
     dispatch({
